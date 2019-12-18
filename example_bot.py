132145b0f94ec5dc774af6bd7ead0680dfe7caad
Matthew Alvero <matthewadrianoalvero@gmail.com>
2019-12-17 18:56:48 -0600

    Update example_bot.py

--- example_bot.py
+++ example_bot.py
@@ -9,6 +9,7 @@ joined = 0
 messages = 0
 leaving = 0
 
+
 # logging statistics of the server into stats.txt
 async def update_stats():
     await client.wait_until_ready()
@@ -28,19 +29,22 @@ async def update_stats():
             print(e)
             await asyncio.sleep(86400)
 
+
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
+
 
 @client.event
 async def on_member_join(member):
     global joined
     joined += 1
     for channel in member.guild.channels:
-        if str(channel) == "normal": # check that we are sending in normal channel
+        if str(channel) == "normal":  # check that we are sending in normal channel
             await channel.send_message(f"""Welcome to the server :b:ruh {member.mention}""")
     # print to console
     print(f'{member} has joined the server')
+
 
 @client.event
 async def on_member_remove(member):
@@ -57,26 +61,24 @@ async def on_member_remove(member):
 async def on_message(message):
     global messages
     messages += 1
-    id = client.get_guild(201857474855305217)
-    print(message.content) # Log all messages into console
+    print(message.content)  # Log all messages into console
 
-    if message.content.find("!hello") != -1:
-        await message.channel.send("What's god :b:ruh!")
-    elif message.content == "!users":
-        await message.channel.send(f"""# of :b:ruhs in the server: {id.member_count}""")
 
 @client.command()
 async def bing(ctx):
     await ctx.send(':b:ong!')
 
+
 @client.command()
 async def hello(ctx):
     await ctx.send("What's good :b:ruh!")
 
+
 @client.command()
 async def users(ctx):
-    id = ctx.guild
-    await ctx.send(f"""# of :b:ruhs in the server: {id.member_count}""")
+    guild_id = ctx.guild
+    await ctx.send(f"""# of :b:ruhs in the server: {guild_id.member_count}""")
+
 
 client.loop.create_task(update_stats())
-client.run("NTI3Njk4Njk5MjgzODU3NDA5.Xfkegw.OiQiYaEneSuWHLPkET-OpwPe2cI")
+client.run('NTI3Njk4Njk5MjgzODU3NDA5.Xfkegw.OiQiYaEneSuWHLPkET-OpwPe2cI')
